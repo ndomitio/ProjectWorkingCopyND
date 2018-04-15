cacfa4b8a5a183ee28f06d41f405d4e533189dd1
NicholasDomitio <nicholas.domitio@gmail.com>
2018-04-15 14:37:58 -0400

    first

---
+++ PhytRxProject/Content/script.js
@@ -0,0 +1,19 @@
+﻿.(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+var fsClient = filestack.init('A2SRemgNRSCQt4ifZoCbUz'); //api key
+function openPicker() {
+    fsClient.pick({
+        accept: ['image/*'],
+        minFiles: 1,
+        maxFiles: 1,
+        maxSize: (100 * 1024 * 1024)
+    }).then(function (response) {
+        console.log(response);
+        $('#exPhoto').attr("value", response.filesUploaded[0].url);
+        var permImage = response.filesUploaded[0].url;
+    });
+}
+
+
+Add CommentCollapse 
---
+++ PhytRxProject/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using PhytRxProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhytRxProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ViewBag.Name = user.Name;
+
+                ViewBag.displayMenu = "No";
+
+                if (isAdminUser())
+                {
+                    ViewBag.displayMenu = "Yes";
+                }
+                return View();
+            }
+            else
+            {
+                ViewBag.Name = "Not Logged IN";
+            }
+            return View();
+
+
+        }
+
+        public Boolean isAdminUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ApplicationDbContext context = new ApplicationDbContext();
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var s = UserManager.GetRoles(user.GetUserId());
+                if (s[0].ToString() == "Admin")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+
+    }
+
+}
